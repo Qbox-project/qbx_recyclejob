c1d2e811d5a7bd683fc0937c9b87c505e19be573
shanruto <dalavanja025@gmail.com>
2023-11-22 14:13:46 +0100

    refactor: servera.lua move to qbox and ox

--- server/main.lua
+++ server/main.lua
@@ -1,29 +1,24 @@
-local QBCore = exports['qbx-core']:GetCoreObject()
+local config = require 'config.server'
 
--- Events
+RegisterNetEvent('qbx_recycle:server:getItem', function()
+    local src = source
 
-RegisterNetEvent('qb-recycle:server:getItem', function()
-  local src = source
-  local Player = QBCore.Functions.GetPlayer(src)
-  for _ = 1, math.random(1, Config.MaxItemsReceived), 1 do
-    local randItem = Config.ItemTable[math.random(1, #Config.ItemTable)]
-    local amount = math.random(Config.MinItemReceivedQty, Config.MaxItemReceivedQty)
-    Player.Functions.AddItem(randItem, amount)
-    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[randItem], 'add')
-    Wait(500)
-  end
+    for _ = 1, math.random(1, config.maxItemsReceived), 1 do
+        local randItem = config.itemTable[math.random(1, #config.itemTable)]
+        local amount = math.random(config.minItemReceivedQty, config.maxItemReceivedQty)
+        exports.ox_inventory:AddItem(src, randItem, amount)
+        Wait(500)
+    end
 
-  local chance = math.random(1, 100)
-  if chance < 7 then
-    Player.Functions.AddItem(Config.ChanceItem, 1, false)
-    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.ChanceItem], 'add')
-  end
+    local chance = math.random(1, 100)
+    if chance < 7 then
+        exports.ox_inventory:AddItem(src, config.chanceItem, 1)
+    end
 
-  local luck = math.random(1, 10)
-  local odd = math.random(1, 10)
-  if luck == odd then
-    local random = math.random(1, 3)
-    Player.Functions.AddItem(Config.LuckyItem, random)
-    TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[Config.LuckyItem], 'add')
-  end
+    local luck = math.random(1, 10)
+    local odd = math.random(1, 10)
+    if luck == odd then
+        local random = math.random(1, 3)
+        exports.ox_inventory:AddItem(src, config.luckyItem, random)
+    end
 end)
